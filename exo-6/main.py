a4c4d1c4c03743cb4329ed3c2e9fbe547634bbd2
unknown <flematthias@gmail.com>
2020-03-23 17:12:57 +0100

    adeadezd

--- exo-6/main.py
+++ exo-6/main.py
@@ -1,12 +1,24 @@
 import json
 
-d = open('doc.json', 'r+', encoding='utf-8', newline="")
-# print(d.read())
+# d = open('doc.json', 'r+', encoding='utf-8', newline="")
+# print(json.load(d))
 
-for entry in d:
+# for entry in json.load(d):
+#     sourceFile = open('python.txt', 'a')
+#     print(entry['members'][0]['name'], file = sourceFile)
+#     print(entry.get('formed'))
+#     sourceFile.close()
+
+with open('doc.json') as f:
+    data = json.load(f)
+
+key = 0
+
+for el in data :
     sourceFile = open('python.txt', 'a')
-    print(entry, file = sourceFile)
-    print(entry)
+    print(data['members'][key]['name'], file = sourceFile)
     sourceFile.close()
+    print(data['members'][key]['name'])
+    key += 1
 
 
