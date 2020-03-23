6e211af8eee15e1d77699e641544bc0f8fe73a59
unknown <flematthias@gmail.com>
2020-03-23 16:35:05 +0100

    exo6

--- exo-6/main.py
+++ exo-6/main.py
@@ -1,6 +1,12 @@
 import json
 
-f = open('doc.json', 'r+', encoding='utf-8', newline="")
-print(f.read())
-for entry in json.load(f.read()):
+d = open('doc.json', 'r+', encoding='utf-8', newline="")
+# print(d.read())
+
+for entry in d:
+    sourceFile = open('python.txt', 'a')
+    print(entry, file = sourceFile)
     print(entry)
+    sourceFile.close()
+
+
