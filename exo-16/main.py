56adf378ef7dd92cc89c0587efa2fa2a23a11197
flematthias <flematthias@gmail.com>
2020-03-25 11:42:10 +0100

    rien

--- exo-16/main.py
+++ exo-16/main.py
@@ -1,14 +1,20 @@
 import logging
 
+logging.basicConfig()
+log = logging.getLogger("MyClass")
+log.setLevel(logging.CRITICAL)
+log.warning('Attention')
+log.error('erreur')
+log.critical('critique')
+log.log(50, "coucou")
+
 
 class MyClass:
     @staticmethod
     def staticmethod(up):
-        logging.warning('Attention')
-        logging.error('erreur')
-        logging.critical('critique')
-        logging.log(50, "coucou")
+
         return up.upper()
 
 
 print(MyClass.staticmethod('stringtoupper'))
+
