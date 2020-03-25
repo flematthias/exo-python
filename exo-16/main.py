48707d095e0301f0ad7231c98c2e7f6be57ac9f8
flematthias <flematthias@gmail.com>
2020-03-25 11:27:46 +0100

    rien

--- exo-16/main.py
+++ exo-16/main.py
@@ -1,11 +1,13 @@
 import logging
 
+
 class MyClass:
     @staticmethod
     def staticmethod(up):
         logging.warning('Attention')
         logging.error('erreur')
         logging.critical('critique')
+        logging.log(50, "coucou")
         return up.upper()
 
 
